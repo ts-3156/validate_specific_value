6a8c363a4646e09e7d63edc81ae5d0de6e785b2f
ts-3156 <ts_3156@yahoo.co.jp>
2018-12-21 04:22:57 +0900

    Update Rakefile

--- test/validate_specific_test.rb
+++ test/validate_specific_test.rb
@@ -6,21 +6,21 @@ class ValidateSpecificValueTest < Minitest::Test
   end
 
   def test_instance_methods
-    user = User.create!(name: 'name')
+    User.create!(name: 'name')
 
     assert User.new(name: 'name2').valid_name?
     assert !User.new(name: 'name').valid_name?
   end
 
   def test_class_methods
-    user = User.create!(name: 'name')
+    User.create!(name: 'name')
 
     assert User.valid_name?('name2')
     assert !User.valid_name?('name')
   end
 
   def test_assign_errors
-    user = User.create!(name: 'name')
+    User.create!(name: 'name')
 
     errors = {}
     assert !User.valid_name?('name', errors)
